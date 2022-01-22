7387bd579eb381839241347ab5afc9a577068fce
Niall Murphy <leturf@gmail.com>
2022-01-22 16:19:23 +0700

    Added strapi domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const redirects = require('./lib/redirects');
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['http://localhost:1337', 'localhost', 'https://aovuastrapi.niallmurphy.dev'],
+		domains: ['http://localhost:1337', 'localhost', 'aovuastrapi.niallmurphy.dev'],
 	},
 	redirects,
 };
