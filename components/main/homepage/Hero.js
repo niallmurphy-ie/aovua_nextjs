d735f8ac8186820842da7d19df7df128e52785f0
Niall Murphy <leturf@gmail.com>
2022-01-22 19:41:21 +0700

    Hero text added

--- components/main/homepage/Hero.js
+++ components/main/homepage/Hero.js
@@ -51,6 +51,18 @@ const Hero = (props) => {
 									layout="fill"
 									objectFit="cover"
 								/>
+								<div className="container">
+									<div className="row">
+										<div className="col col-lg-8 col-md-12 col-12 slide-caption">
+											<div className="slide-title">
+												<h2>{image.alternativeText}</h2>
+											</div>
+											<div className="slide-subtitle">
+												<p>{image.caption}</p>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					))}
