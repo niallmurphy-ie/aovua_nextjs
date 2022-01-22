5f0e17bc8dd267ece2eb210bb28211490ef9c874
Niall Murphy <leturf@gmail.com>
2022-01-22 19:17:36 +0700

    Hero image changed to next/image

--- components/main/homepage/Hero.js
+++ components/main/homepage/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Image from 'next/image';
 
 const Hero = (props) => {
 	if (!props.heroImages) return null;
@@ -21,25 +22,35 @@ const Hero = (props) => {
 			<div className="hero-slider">
 				<Slider {...settings}>
 					{props.heroImages.map((image, index) => (
-						<div key={index} className="slide">
-							<div
-								className="slide-inner"
-								style={{
-									backgroundImage: `url(${process.env.NEXT_PUBLIC_STRAPI_URL}${image.url})`,
-								}}
-							>
-								<div className="container">
-									<div className="row">
-										<div className="col col-lg-8 col-md-12 col-12 slide-caption">
-											<div className="slide-title">
-												<h2>{image.alternativeText}</h2>
-											</div>
-											<div className="slide-subtitle">
-												<p>{image.caption}</p>
-											</div>
-										</div>
-									</div>
-								</div>
+						// <div key={index} className="slide">
+						// 	<div
+						// 		className="slide-inner"
+						// 		style={{
+						// 			backgroundImage: `url(${process.env.NEXT_PUBLIC_STRAPI_URL}${image.url})`,
+						// 		}}
+						// 	>
+						// 		<div className="container">
+						// 			<div className="row">
+						// 				<div className="col col-lg-8 col-md-12 col-12 slide-caption">
+						// 					<div className="slide-title">
+						// 						<h2>{image.alternativeText}</h2>
+						// 					</div>
+						// 					<div className="slide-subtitle">
+						// 						<p>{image.caption}</p>
+						// 					</div>
+						// 				</div>
+						// 			</div>
+						// 		</div>
+						// 	</div>
+						// </div>
+						<div key={`hero_${index}`} className="slide">
+							<div className="slide-inner">
+								<Image
+									src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${image.url}`}
+									alt={image.alternativeText}
+									layout="fill"
+									objectFit="cover"
+								/>
 							</div>
 						</div>
 					))}
