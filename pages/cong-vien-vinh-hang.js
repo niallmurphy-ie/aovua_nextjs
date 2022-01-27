2c71686e5cd147050e681f747ec5658869bdf3c7
Niall Murphy <leturf@gmail.com>
2022-01-27 17:19:16 +0700

    Added Ngoc Xanh and Dam Long

---
+++ pages/cong-vien-vinh-hang.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import Location from '../components/main/location/Location';
+import { LOCATION } from '../lib/queries/locationQueries';
+import client from '../lib/apolloClient';
+
+export default function DaoNgocXanh({ location }) {
+	return (
+		<>
+			<Head>
+				<title>{location.Name}</title>
+				<meta
+					name="description"
+					content="Công viên Vĩnh Hằng meta description #toDO"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Layout home={false}>
+				<Location location={location} />
+			</Layout>
+		</>
+	);
+}
+
+export async function getStaticProps() {
+	const { data } = await client.query({
+		query: LOCATION,
+		variables: {
+			id: 6,
+		},
+	});
+
+	return {
+		props: {
+			location: data.location,
+		},
+	};
+}
---
+++ pages/dam-long.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import Location from '../components/main/location/Location';
+import { LOCATION } from '../lib/queries/locationQueries';
+import client from '../lib/apolloClient';
+
+export default function DaoNgocXanh({ location }) {
+	return (
+		<>
+			<Head>
+				<title>{location.Name}</title>
+				<meta
+					name="description"
+					content="Khu du lịch Đầm Long meta description #toDO"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Layout home={false}>
+				<Location location={location} />
+			</Layout>
+		</>
+	);
+}
+
+export async function getStaticProps() {
+	const { data } = await client.query({
+		query: LOCATION,
+		variables: {
+			id: 5,
+		},
+	});
+
+	return {
+		props: {
+			location: data.location,
+		},
+	};
+}
