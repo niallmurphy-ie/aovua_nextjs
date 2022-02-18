be2c9fc453f8facbbf7024714a9ec3af649fb3dc
Niall Murphy <leturf@gmail.com>
2022-02-18 20:13:44 +0700

    Experimental features

--- next.config.js
+++ next.config.js
@@ -8,8 +8,15 @@ const redirects = require('./lib/redirects');
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['http://localhost:1337', 'localhost', 'aovuastrapi.niallmurphy.dev'],
+		domains: [
+			'http://localhost:1337',
+			'localhost',
+			'aovuastrapi.niallmurphy.dev',
+		],
 		// minimumCacheTTL: 31536000,
 	},
 	redirects,
+	experimental: {
+		concurrentFeatures: true,
+	},
 };
