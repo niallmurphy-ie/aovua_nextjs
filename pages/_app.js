5a44f84bce52a367a58ee283e260ca9911077ab3
Niall Murphy <leturf@gmail.com>
2022-02-19 23:03:03 +0700

    Removed og:image from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -48,12 +48,12 @@ export default function App({ Component, pageProps, footerData, ...appProps }) {
 			<div className="App">
 				<Head>
 					<link rel="icon" href="/favicon.ico" />
-					<meta
+					{/* <meta
 						property="og:image"
 						content={`https://og-image.vercel.app/${encodeURI(
 							'Ao Vua'
 						)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-					/>
+					/> */}
 					<meta name="og:title" content="Ao Vua" />
 					<meta name="twitter:card" content="summary_large_image" />
 				</Head>
