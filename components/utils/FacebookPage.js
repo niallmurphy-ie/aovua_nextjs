512ffa8248df2e09c0043eccb5e8a7d7fd8c1d39
Niall Murphy <leturf@gmail.com>
2022-02-21 17:44:05 +0700

    Facebook fix attempt 1

--- components/utils/FacebookPage.js
+++ components/utils/FacebookPage.js
@@ -1,19 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
 const FacebookPage = ({ url, name }) => {
+	const [facebookURL, facebookMapURL] = useState(null);
+
+	useEffect(() => {
+		facebookMapURL(url);
+		window.document.dispatchEvent(
+			new Event('DOMContentLoaded', {
+				bubbles: true,
+				cancelable: true,
+			})
+		);
+	}, []);
+
+	if (!facebookURL) return null;
+
 	return (
 		<div
 			className="fb-page"
-			data-href={url}
+			data-href={facebookURL}
 			data-tabs="timeline"
-			data-width="700"
+			data-width="500"
 			data-height="400"
 			data-small-header="true"
 			data-adapt-container-width="true"
 			data-hide-cover="true"
 			data-show-facepile="true"
-			data-lazy="true"
+			data-lazy="false"
 		>
-			<blockquote cite={url} className="fb-xfbml-parse-ignore">
-				<a href={url}>{name}</a>
+			<blockquote cite={facebookURL} className="fb-xfbml-parse-ignore">
+				<a href={facebookURL}>{name}</a>
 			</blockquote>
 		</div>
 	);
