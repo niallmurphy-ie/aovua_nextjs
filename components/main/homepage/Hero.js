3b2b1a2b7ba759a529d42495bf9de28a66e76c04
Niall Murphy <leturf@gmail.com>
2022-02-24 14:39:03 +0700

    Decresed Hero slide delay

--- components/main/homepage/Hero.js
+++ components/main/homepage/Hero.js
@@ -16,7 +16,7 @@ const Hero = ({ heroImages }) => {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 7500,
+		autoplaySpeed: 4500,
 		fade: true,
 		lazyLoad: 'progressive',
 	};
