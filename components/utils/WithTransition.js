965105833b37c875bd6ad359ff7469480640f3b7
Niall Murphy <leturf@gmail.com>
2022-02-24 21:50:58 +0700

    Reduced animation x

--- components/utils/WithTransition.js
+++ components/utils/WithTransition.js
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 const WithTransition = ({ children }) => {
 	const variants = {
-		hidden: { opacity: 0, x: -200, y: 0 },
+		hidden: { opacity: 0, x: -20, y: 0 },
 		enter: { opacity: 1, x: 0, y: 0 },
 		exit: { opacity: 0, x: 0, y: -100 },
 	};
