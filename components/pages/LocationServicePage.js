000a35980f138b7d2e642cdfa0150d6b90741745
Niall Murphy <leturf@gmail.com>
2022-03-02 17:30:00 +0700

    Entertainment page added to location

---
+++ components/pages/LocationServicePage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FadeInWhenVisible } from '../utils/Animations';
+
+const LocationServicePage = ({ location, entertainments }) => {
+	// Columns 3 wide
+	let content;
+	if (entertainments) content = entertainments
+	return (
+		<div className="container-fluid">
+			<div className="row gallery-wrap">
+				{content && content.map((cont, index) => {
+					return (
+						<div
+							key={`hero_location_${index}`}
+							className="col-md-3"
+						>
+							<FadeInWhenVisible
+								initialScale={0.95}
+							>
+								<div className="gallery-item">
+									<div className="gallery-img">
+										<Image
+											src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${cont.Thumbnail.url}`}
+											alt={`Hình ảnh của ${cont.Name}`}
+											height={400}
+											width={500}
+											objectFit="cover"
+											loading={
+												index === 0 ? 'eager' : 'lazy'
+											}
+										/>
+									</div>
+									<div className="gallery-content">
+										<div className="content-left">
+											<h5>
+												<Link
+													href={`/${location.urlPrefix}/${cont.slug}`}
+												>
+													<a>{cont.Name}</a>
+												</Link>
+											</h5>
+										</div>
+									</div>
+								</div>
+							</FadeInWhenVisible>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default LocationServicePage;
