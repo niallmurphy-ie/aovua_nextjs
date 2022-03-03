7e1c747fde721ed88906cbfb54b08ddf646ec05e
Niall Murphy <leturf@gmail.com>
2022-03-03 15:28:17 +0700

    Added sightseeings to locations

--- components/pages/LocationServicePage.js
+++ components/pages/LocationServicePage.js
@@ -3,7 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FadeInWhenVisible } from '../utils/Animations';
 
-const LocationServicePage = ({ location, entertainments }) => {
+const LocationServicePage = ({ location, entertainments, sightseeings }) => {
+
+	console.log('sightseeings :>> ', sightseeings);
 	// Columns 3 wide
 	let content;
 	let urlFragement;
@@ -11,47 +13,52 @@ const LocationServicePage = ({ location, entertainments }) => {
 		content = entertainments;
 		urlFragement = 'vui-choi-giai-tri';
 	}
+	if (sightseeings) {
+		content = sightseeings;
+		urlFragement = 'cac-diem-tham-quan';
+	}
 	return (
 		<div className="container-fluid">
 			<div className="row gallery-wrap">
-				{content && content.map((cont, index) => {
-					return (
-						<div
-							key={`hero_location_${index}`}
-							className="col-md-3"
-						>
-							<FadeInWhenVisible
-								initialScale={0.95}
+				{content &&
+					content.map((cont, index) => {
+						return (
+							<div
+								key={`hero_location_${index}`}
+								className="col-md-3"
 							>
-								<div className="gallery-item">
-									<div className="gallery-img">
-										<Image
-											src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${cont.Thumbnail.url}`}
-											alt={`Hình ảnh của ${cont.Name}`}
-											height={400}
-											width={500}
-											objectFit="cover"
-											loading={
-												index === 0 ? 'eager' : 'lazy'
-											}
-										/>
-									</div>
-									<div className="gallery-content">
-										<div className="content-left">
-											<h5>
-												<Link
-													href={`/${location.urlPrefix}/${urlFragement}/${cont.slug}`}
-												>
-													<a>{cont.Name}</a>
-												</Link>
-											</h5>
+								<FadeInWhenVisible initialScale={0.95}>
+									<div className="gallery-item">
+										<div className="gallery-img">
+											<Image
+												src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${cont.Thumbnail.url}`}
+												alt={`Hình ảnh của ${cont.Name}`}
+												height={400}
+												width={500}
+												objectFit="cover"
+												loading={
+													index === 0
+														? 'eager'
+														: 'lazy'
+												}
+											/>
+										</div>
+										<div className="gallery-content">
+											<div className="content-left">
+												<h5>
+													<Link
+														href={`/${location.urlPrefix}/${urlFragement}/${cont.slug}`}
+													>
+														<a>{cont.Name}</a>
+													</Link>
+												</h5>
+											</div>
 										</div>
 									</div>
-								</div>
-							</FadeInWhenVisible>
-						</div>
-					);
-				})}
+								</FadeInWhenVisible>
+							</div>
+						);
+					})}
 			</div>
 		</div>
 	);
