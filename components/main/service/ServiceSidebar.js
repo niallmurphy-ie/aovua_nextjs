0787cb46e8c357d00aef8c8834a256e117d5cd10
Niall Murphy <leturf@gmail.com>
2022-03-07 15:01:02 +0700

    Small change to image format

--- components/main/service/ServiceSidebar.js
+++ components/main/service/ServiceSidebar.js
@@ -11,29 +11,37 @@ const ServiceSidebar = ({ location, type, typeURL, services }) => {
 				<div className="widget recent-post-widget">
 					<h3>{`${type} tại ${location.Name}`}</h3>
 					<div className="posts">
-						{services.map((service, index) => (
-							<div key={index + service.Title} className="post">
-								<div className="img-holder">
-									<Image
-										src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${service.Thumbnail.formats.thumbnail.url}`}
-										alt=""
-										width="100%"
-										height="100%"
-										layout="responsive"
-										objectFit="cover"
-									/>
+						{services.map((service, index) => {
+							const imageURL = service.Thumbnail.formats
+								? service.Thumbnail.formats.thumbnail.url
+								: service.Thumbnail.url;
+							return (
+								<div
+									key={index + service.Title}
+									className="post"
+								>
+									<div className="img-holder">
+										<Image
+											src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${imageURL}`}
+											alt=""
+											width="100%"
+											height="100%"
+											layout="responsive"
+											objectFit="cover"
+										/>
+									</div>
+									<div className="details">
+										<h4>
+											<Link
+												href={`/${location.urlPrefix}/${typeURL}/${service.slug}`}
+											>
+												<a>{service.Name}</a>
+											</Link>
+										</h4>
+									</div>
 								</div>
-								<div className="details">
-									<h4>
-										<Link
-											href={`/${location.urlPrefix}/${typeURL}/${service.slug}`}
-										>
-											<a>{service.Name}</a>
-										</Link>
-									</h4>
-								</div>
-							</div>
-						))}
+							);
+						})}
 						{services.length > 5 && (
 							<div className="view-all blog-content">
 								<Link
