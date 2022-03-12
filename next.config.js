a7d2750a07c2bb00825f61e3c2997962ceae95e2
Niall Murphy <leturf@gmail.com>
2022-03-12 19:19:52 +0700

    Increased cache for images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 const redirects = require('./lib/redirects');
-const { withPlaiceholder } = require('@plaiceholder/next');
 
 // const withSass = require('@zeit/next-sass');
 // const withCSS = require('@zeit/next-css');
 
 // module.exports = withCSS(withSass());
 
-module.exports = withPlaiceholder({
+module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: [
@@ -14,10 +13,10 @@ module.exports = withPlaiceholder({
 			'localhost',
 			'aovuastrapi.niallmurphy.dev',
 		],
-		// minimumCacheTTL: 31536000,
+		minimumCacheTTL: 31536000,
 	},
 	redirects,
 	experimental: {
 		concurrentFeatures: true,
 	},
-});
+};
