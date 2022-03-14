e0c9ed39ba26724bf9597b0c647dcff834ea857b
Niall Murphy <leturf@gmail.com>
2022-03-14 15:26:01 +0700

    Messenger tests

--- components/utils/FacebookMessenger.js
+++ components/utils/FacebookMessenger.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-const FacebookMessenger = () => (
-	<MessengerCustomerChat
-		pageId="1608577759385364"
-		appId=""
-		htmlRef="https://aovua.niallmurphy.dev"
-		themeColor="#1c4c25"
-		greetingDialogDisplay="fade"
-		// loggedInGreeting="loggedInGreeting"
-		// loggedOutGreeting="loggedOutGreeting"
-		language="vn_VN"
-	/>
-);
+const FacebookMessenger = () => {
+	const locations = [
+		{
+			name: 'Ao Vua',
+			pageID: '751126184998127',
+		},
+		{
+			name: 'Đảo Ngọc Xanh',
+			pageID: '1608577759385364',
+		},
+	];
+
+	const [isOpen, setIsOpen] = useState(false);
+	const [location, setLocation] = useState(locations[0].pageID);
+
+	return (
+		<>
+			<div className="messenger-locations">
+				{locations.map((item, index) => (
+					<button
+						key={`messengerLocation_${index}`}
+						className={`messenger-location ${
+							location === item.pageID ? 'active' : ''
+						}`}
+						onClick={() => {
+							setLocation(item.pageID);
+						}}
+					>
+						{item.name}
+					</button>
+				))}
+			</div>
+			<MessengerCustomerChat
+				pageId={location}
+				appId=""
+				htmlRef="https://aovua.niallmurphy.dev"
+				themeColor="#1c4c25"
+				greetingDialogDisplay="fade"
+				// loggedInGreeting="loggedInGreeting"
+				// loggedOutGreeting="loggedOutGreeting"
+				language="vn_VN"
+				onCustomerChatDialogShow={() => {
+					setIsOpen(true);
+					console.log('onCustomerChatDialogShow');
+				}}
+				onCustomerChatDialogHide={() => {
+					setIsOpen(false);
+					console.log('onCustomerChatDialogHide');
+				}}
+			/>
+		</>
+	);
+};
 
 export default FacebookMessenger;
